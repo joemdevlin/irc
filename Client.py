c3ce37b61749bdc4ebfab8c19ba50927886f98cc
Joseph M Devlin <joseph.m.devlin@intel.com>
2021-02-11 11:14:37 -0800

    Can send and recieve messages

--- Client.py
+++ Client.py
@@ -1,26 +1,26 @@
 import socket  
 import select  
 import sys  
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+import _thread
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
 ip = 'localhost'  
-port = 4781 
+port = 4137 
 server.connect((ip, port))  
-  
-while True:  
-  
-    # maintains a list of possible input streams  
-    sockets_list = [sys.stdin, server]  
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])  
-  
-    for socks in read_sockets:  
-        if socks == server:  
-            message = socks.recv(2048)  
-            print (message.decode('utf-8'))  
-        else:  
-            message = sys.stdin.readline()  
-            server.send(message)  
-            sys.stdout.write("<You>")  
-            sys.stdout.write(message)  
-            sys.stdout.flush()  
+print(server)
+
+def userInterface():
+    while True:
+        message = sys.stdin.readline()  
+        server.send(message.encode('utf-8')) 
+
+_thread.start_new_thread(userInterface,()) 
+
+while True:   
+    data = server.recv(2048)
+    if data:   
+        print (data.decode('utf-8')) 
+
 server.close()  
--- Server.py
+++ Server.py
@@ -7,56 +7,56 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
     
 ip = 'localhost'  
-port = 4781
+port = 4137
 server.bind((ip, port))  
 server.listen(10)  
   
 clients = []  
   
 # Spawning a serperate thread per conenction to relay the message
-def clientthread(conn, addr):  
-    conn.send("You there?".encode('utf-8'))  
-  
+def clientthread(conn, addr):
     while True:  
             try:  
                 data = conn.recv(2048)  
                 if data: 
-                    msg = "<" + addr[0] + "> " + data
-                    print (msg)  
-                    broadcast(msg, conn)  
-  
-                else:  
+                    msg = "<" + addr[0] + "> " + data.decode('utf-8')
+                    broadcast(msg.encode('utf-8'), conn)
+                else: 
                     disconnect(conn)  
   
-            except: 
-                print("Error?") 
+            except socket.error as e:
+                print("ERROR: ") 
+                print(e) 
                 continue
   
 def broadcast(message, connection):  
     for client in clients:  
-        if client!=connection:  
-            try:  
-                client.send(message)  
-            except:  
-                client.close()   
-                disconnect(client)  
+        try:  
+            client.send(message) 
+            print("Message Sent") 
+        except Exception as e:
+            print("ERROR: ") 
+            print(e)  
+            client.close()   
+            disconnect(client)  
 
-def disconnect(connection):  
+def disconnect(client):  
     if client in clients:  
-        client.remove(client)  
+        clients.remove(client)  
 
 if __name__ == '__main__':  
     try:
         while True:  
             conn, addr = server.accept()  
+            print(conn)
             clients.append(conn)  
             print (addr[0] + " connected") 
         
             # creates and individual thread for every user  
             # that connects  
             _thread.start_new_thread(clientthread,(conn,addr)) 
+            clientthread(conn, addr)
                     
-            conn.close()  
-            server.close()  
     except KeyboardInterrupt:
         pass
+    server.close()  
